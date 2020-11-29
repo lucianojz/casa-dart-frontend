cf2475807e6344907aa39b5683c8f702440e73ed
Luciano Janzkovski <luccas.janzkovski@gmail.com>
2020-11-29 12:11:44 -0300

    New Customer Form Page

---
+++ src/pages/Customer/index.tsx
@@ -0,0 +1,84 @@
+import React, { useRef, useCallback } from 'react';
+
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
+import Layout from '../../components/Layout';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  CustomerBody,
+  CustomerDetail,
+  FormFields,
+  FormFooter,
+} from './styles';
+
+const Customer: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleAddCustomer = useCallback(() => {
+    console.log('okey');
+  }, []);
+
+  return (
+    <Layout>
+      <Container>
+        <CustomerBody>
+          <CustomerDetail>
+            <Form ref={formRef} onSubmit={handleAddCustomer}>
+              <FormFields>
+                <Input
+                  className="InputName"
+                  name="full_name"
+                  type="text"
+                  label="Nome Completo"
+                />
+
+                <Input
+                  className="InputEmail"
+                  name="email"
+                  type="text"
+                  label="Email"
+                />
+
+                <Input name="password" type="password" label="Senha" />
+
+                <Input name="document" type="text" label="CPF" />
+
+                <Input name="phone" type="text" label="Telefone" />
+
+                <Input
+                  className="InputAddress"
+                  name="address"
+                  type="text"
+                  label="Endereço"
+                />
+
+                <Input
+                  className="InputAddress"
+                  name="address_complement"
+                  type="text"
+                  label="Complemento"
+                />
+
+                <Input name="postal_code" type="text" label="CEP" />
+              </FormFields>
+              <FormFooter>
+                <Button type="button" variation="error">
+                  CANCELAR CADASTRO
+                </Button>
+                <Button type="submit" variation="success">
+                  CADASTRAR
+                </Button>
+              </FormFooter>
+            </Form>
+          </CustomerDetail>
+        </CustomerBody>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Customer;
---
+++ src/pages/Customer/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 900px;
+  padding: 10px 0 10px;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const CustomerBody = styled.div`
+  width: 800px;
+  min-height: 300px;
+
+  padding: 10px;
+
+  background: #fff;
+
+  border: #231f20 solid 1px;
+  border-radius: 5px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CustomerDetail = styled.div`
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .InputName,
+  .InputEmail,
+  .InputAddress {
+    width: 400px;
+  }
+`;
+
+export const FormFields = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const FormFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  .Button {
+    margin-left: 10px;
+  }
+`;
