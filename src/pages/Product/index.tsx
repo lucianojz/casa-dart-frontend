f0a8cc26cbd358aeb1d92cddea5eb9e81dcac883
Luciano Janzkovski <luccas.janzkovski@gmail.com>
2020-11-29 08:00:12 -0300

    Product Page

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 
+import CanecaImageL from '../../assets/caneca01-L.png';
+
+import {
+  Container,
+  ProductBody,
+  ProductDetail,
+  ProductImage,
+  ProductBuy,
+} from './styles';
+
 const Product: React.FC = () => {
   return (
     <>
       <Layout>
-        <h1>Produto</h1>
+        <Container>
+          <ProductBody>
+            <ProductDetail>
+              <h1>Caneca Artesanal em porcelana - Porcelani Decor</h1>
+              <h3>Detalhes do produto:</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                tincidunt lacus aliquet, fermentum est nec, finibus sem. Integer
+                vitae elementum magna. Morbi ornare elit nec luctus eleifend.
+                Mauris sit amet fermentum urna. Cras consequat ultricies nibh at
+                egestas. Aenean gravida lectus et ipsum ultricies, non venenatis
+                dolor condimentum. Nunc ex diam, imperdiet sit amet felis vitae,
+                lacinia fringilla erat. Curabitur sapien lorem, cursus ac
+                elementum et, sollicitudin et est.
+              </p>
+              <ProductBuy>
+                <strong>R$ 15,00</strong>
+                <a href="/">COMPRAR</a>
+              </ProductBuy>
+            </ProductDetail>
+            <ProductImage>
+              <img src={CanecaImageL} alt="" />
+            </ProductImage>
+          </ProductBody>
+        </Container>
       </Layout>
     </>
   );
--- src/pages/Product/styles.ts
+++ src/pages/Product/styles.ts
@@ -1,3 +1,78 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 900px;
+  padding: 10px 0 10px;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProductBody = styled.div`
+  width: 800px;
+  min-height: 300px;
+
+  padding: 10px;
+
+  background: #fff;
+
+  border: #231f20 solid 1px;
+  border-radius: 5px;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ProductDetail = styled.div`
+  height: 300px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  padding: 15px;
+
+  p {
+    font-weight: 300;
+    font-size: 16px;
+  }
+`;
+
+export const ProductBuy = styled.div`
+  /* width: 200px; */
+
+  display: flex;
+  align-content: flex-end;
+  justify-content: flex-end;
+
+  align-items: center;
+
+  strong {
+    font-weight: 700;
+    font-size: 20px;
+    margin-right: 20px;
+  }
+
+  a {
+    width: 130px;
+    height: 34px;
+
+    background: #ead2ac;
+    color: #000;
+    border-radius: 5px;
+
+    text-decoration: none;
+    font-size: 20px;
+    font-weight: 700;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const ProductImage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
